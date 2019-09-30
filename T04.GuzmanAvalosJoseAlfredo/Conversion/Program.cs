3087d5844b57b9acc45479bb676fd4e4f3b825e6
PyshO <55770642+PyshO@users.noreply.github.com>
2019-09-30 18:56:48 -0500

    Add files via upload

---
+++ T04.GuzmanAvalosJoseAlfredo/Conversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Total_Centavo;
+            int Pesos = 0;
+            int Centavos = 0;
+
+            Solucion Co = new Solucion();
+            
+
+            Console.WriteLine("Bienvenido al programa");
+            Console.WriteLine("Ingrese los centavos");
+            Total_Centavo = Convert.ToInt32(Console.ReadLine());
+            Co.Convertir(Total_Centavo, out Pesos, out Centavos);
+            Console.WriteLine("El total en pesos es: " + Pesos);
+            Console.WriteLine("El total en centavos es: " + Centavos);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ T04.GuzmanAvalosJoseAlfredo/Conversion/Solucion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Solucion
+    {
+        public void Convertir(int Total_Centavos, out int Pesos, out int Centavos)
+        {
+            Pesos = Total_Centavos / 100;
+            Centavos = Total_Centavos % 100;
+        }
+    }
+}
