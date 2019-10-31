755f25461e14ef1c2465564363abb73928b6cf6d
PyshO <55770642+PyshO@users.noreply.github.com>
2019-10-31 15:04:45 -0600

    Add files via upload

---
+++ T01.GuzmanAvalosJoseAlfredo/Abstrapto/Figura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstrapto
+{
+    public abstract class Figura
+    {
+        //Creamos los metodos para pedir datos
+        public abstract double Base_1 { get; set; }
+        public abstract double Altura { get; set; }
+    }
+}
---
+++ T01.GuzmanAvalosJoseAlfredo/Abstrapto/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstrapto
+{
+    public class Triangulo:Figura
+    {
+        //Mandamos llamar los metodos de la clase abstracta
+        public override double Base_1 { get; set; }
+        public override double Altura { get; set; }
+
+        //Creamos el metodo para calcular area del tiangulo
+        public double AreaTriangulo(double a, double b)
+        {
+            double res = (a * b) / 2;
+            return res;
+        }
+    }
+}
