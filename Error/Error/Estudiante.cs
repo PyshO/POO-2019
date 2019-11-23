109eb42049a1e4b978a6d5c388471f7561a40e32
PyshO <55770642+PyshO@users.noreply.github.com>
2019-11-23 01:00:47 -0600

    Add files via upload

---
+++ Error/Error/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error
+{
+    public class Estudiante
+    {
+        //Creamos las nuevas variables y las encapsulamos
+        public string Nombre { get; set; }
+        public string Carrera { get; set; }
+        public int Semestre { get; set; }
+        public int Telefono { get; set; }
+        public int NoControl { get; set; }
+    }
+}
---
+++ Error/Error/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos una lista de estudiantes
+            var estudiante = new List<Estudiante>();
+            var agregar = true;
+
+            while (agregar)
+            {
+                //Creamos el try para capturar los errores al no escribir ningun dato
+                try
+                {
+                    //instanciamos la clase de estudiante
+                    var E = new Estudiante();
+
+                    int S = E.NoControl;
+                    bool result;
+
+                    //pedimos al usuario ingresar los datos
+                    Console.WriteLine("Ingrese su nombre:");
+                    E.Nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese su semestre:");
+                    E.Semestre = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingresa tu Carrera::");
+                    E.Carrera = Console.ReadLine();
+                    Console.WriteLine("Ingresa tu telefono:");
+                    E.Telefono = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingresa tu Numero de Control:");
+
+                    //usamos el int.Tryparse por si el usuario escribe algo que no sea numero, en vez de un numero lo convierte a entero y devuelve un 0
+                    //El out sirve para regresar solo un valor
+                    result = int.TryParse(Console.ReadLine(), out S);
+
+                    //hacemos una condicion donde si result guarda un numero sera verdadero si no
+                    if (result == false)
+                    {
+                        Console.WriteLine("solo se permiten numeros, intente de nuevo:");
+                        result = int.TryParse(Console.ReadLine(), out S);
+                    }
+
+                    estudiante.Add(E);
+
+                    Console.WriteLine("¿Desea agregar otro usuario?");
+                    Console.WriteLine("opc 1-.Si");
+                    Console.WriteLine("opc 2-.No");
+                    //hacemos la condicion por si el usuario dice que si seguir con el programa, de lo contrario se cierra
+                    if (Console.ReadLine() != "si")
+                    {
+                        agregar = false;
+                    }
+                }
+
+                //el catch captura errores si no se escrive un valor o diferente al pedido
+                catch (Exception)
+                {
+                    Console.WriteLine("Fallo al agregar usuario, vuelvalo a intentar");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
